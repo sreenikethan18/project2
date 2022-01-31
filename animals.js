29df96f915aefab9c05447960f923c775a1d110e
sreenikethan18 <97608706+sreenikethan18@users.noreply.github.com>
2022-01-31 20:17:44 +0530

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,40 @@
+function lion(){
+    audio = document.getElementById("lion");
+    audio.play()
+}
+function Bear(){
+    audio = document.getElementById("Bear");
+    audio.play()
+}
+function Cat(){
+    audio = document.getElementById("Cat");
+    audio.play()
+}
+function Cow(){
+    audio = document.getElementById("Cow");
+    audio.play()
+}
+function Dog(){
+    audio = document.getElementById("Dog");
+    audio.play()
+}
+function Donkey(){
+    audio = document.getElementById("Donkey");
+    audio.play()
+}
+function Elephant(){
+    audio = document.getElementById("Elephant");
+    audio.play()
+}
+function Hausziege(){
+    audio = document.getElementById("Hausziege");
+    audio.play()
+}
+function Horse(){
+    audio = document.getElementById("Horse");
+    audio.play()
+}
+
+function btn2() {
+    window.location.href = "animalsound.html";
+  }
